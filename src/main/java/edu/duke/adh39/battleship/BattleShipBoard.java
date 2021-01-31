9e81663c5a2875431f66c39aab6d1f2b4d9ffa58
mg476 <mg476@duke.edu>
2021-01-31 22:13:53 +0800

    07

--- src/main/java/edu/duke/adh39/battleship/BattleShipBoard.java
+++ src/main/java/edu/duke/adh39/battleship/BattleShipBoard.java
@@ -1,8 +1,12 @@
 package edu.duke.adh39.battleship;
 
-public class BattleShipBoard implements Board{
+import java.util.ArrayList;
+
+public class BattleShipBoard<T> implements Board<T>{
   private final int width;
   private final int height;
+  private final ArrayList<Ship<T>> myShips;
+  
   public BattleShipBoard(int w, int h) {
     if (w <= 0) {
       throw new IllegalArgumentException("BattleShipBoard's width must be positive but is " + w);
@@ -12,12 +16,37 @@ public class BattleShipBoard implements Board{
     }
     this.width = w;
     this.height = h;
+    this.myShips = new ArrayList<Ship<T>>();
   }
 
+  @Override
   public int getWidth(){
     return width;
   }
+  
+  @Override
   public int getHeight(){
     return height;
   }
+
+  @Override
+  public boolean tryAddShip(Ship<T> toAdd){
+    myShips.add(toAdd);
+    return true;
+  }
+
+  @Override
+  public T whatIsAt(Coordinate where) {
+    for (Ship<T> s: myShips) {
+      if (s.occupiesCoordinates(where)){
+        return s.getDisplayInfoAt(where);
+      }
+    }
+    return null;
+  }
 }
+
+
+
+
+
--- src/test/java/edu/duke/adh39/battleship/BattleShipBoardTest.java
+++ src/test/java/edu/duke/adh39/battleship/BattleShipBoardTest.java
@@ -7,22 +7,57 @@ import org.junit.jupiter.api.Test;
 public class BattleShipBoardTest {
   @Test
   public void test_width_and_height() {
-    Board b1 = new BattleShipBoard(10, 20);
+    Board<Character> b1 = new BattleShipBoard<Character>(10, 20);
     assertEquals(10, b1.getWidth());
     assertEquals(20, b1.getHeight());
   }
 
   @Test
   public void test_invalid_dimensions() {
-    assertThrows(IllegalArgumentException.class, () -> new BattleShipBoard(10, 0));
-    assertThrows(IllegalArgumentException.class, () -> new BattleShipBoard(0, 20));
-    assertThrows(IllegalArgumentException.class, () -> new BattleShipBoard(10, -5));
-    assertThrows(IllegalArgumentException.class, () -> new BattleShipBoard(-8, 20));
-
+    assertThrows(IllegalArgumentException.class, () -> new BattleShipBoard<Character>(10, 0));
+    assertThrows(IllegalArgumentException.class, () -> new BattleShipBoard<Character>(0, 20));
+    assertThrows(IllegalArgumentException.class, () -> new BattleShipBoard<Character>(10, -5));
+    assertThrows(IllegalArgumentException.class, () -> new BattleShipBoard<Character>(-8, 20));
   }
 
+  @Test
+  public void test_try_add_ship() {
+    Board<Character> b1 = new BattleShipBoard<Character>(5, 5);
+    Character expect[][] = { { null, null, null, null, null }, { null, null, null, null, null },
+        { null, null, null, null, null }, { null, null, null, null, null }, { null, null, null, null, null } };
+    checkWhatIsAtBoard(b1, expect);
+    Coordinate c1 = new Coordinate(1,4);
+    Ship<Character> s1 = new BasicShip(c1);
+    Coordinate c2 = new Coordinate(3,1);
+    Ship<Character> s2 = new BasicShip(c2);
+    Coordinate c3 = new Coordinate(0,2);
+    Ship<Character> s3 = new BasicShip(c3);
+    b1.tryAddShip(s1);
+    b1.tryAddShip(s2);
+    b1.tryAddShip(s3);
+    expect[1][4] = 's';
+    expect[3][1] = 's';
+    expect[0][2] = 's';
+    checkWhatIsAtBoard(b1, expect);
+    
+  }
+
+  @Test
+  public void test_what_is_at() {
+    Board<Character> b1 = new BattleShipBoard<Character>(10, 20);
+    Coordinate c1 = new Coordinate("B3");
+    Coordinate c2 = new Coordinate("D5");
+    assertEquals(null, b1.whatIsAt(c1));
+    assertEquals(null, b1.whatIsAt(c2));
+  }
+
+  private <T> void checkWhatIsAtBoard(Board<T> b, T[][] expected){
+    for (int i=0; i<b.getHeight(); i++){
+      for (int j=0; j<b.getWidth(); j++){
+        Coordinate w = new Coordinate(i,j);
+        assertEquals(b.whatIsAt(w), expected[i][j]);
+      }
+    }
+  }
 }
-
-
-
 
